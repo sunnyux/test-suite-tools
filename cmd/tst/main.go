37baa2981594e2204346c4dca353b5b41489f5a4
Matthew Froggatt <mattfr@live.ca>
2019-11-01 22:01:48 -0400

    Made help menu look better, re-added help flag, and began adding variables to store flag values.

--- cmd/tst/main.go
+++ cmd/tst/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"fmt"
 	"github.com/urfave/cli"
-	"log"
 	"os"
 )
 
@@ -10,9 +10,37 @@ func init() {
 }
 
 func main() {
+	// Variables to Store Flag Values
+	//==================================================================================================================
+	var help, rm bool
+	//==================================================================================================================
+	
 	// App Creation - This is all that is needed
 	//==================================================================================================================
 	app := cli.NewApp()
+	//==================================================================================================================
+
+	// App Help Menu Setup - A lot of stuff needs to go here
+	//==================================================================================================================
+	app.Name = "Test Suite Tools"
+	app.Usage = "A set of tools to make CS 246 test suite creation and usage easier."
+	app.Authors = []cli.Author{
+		cli.Author{
+			Name:  "Matthew Froggatt",
+			Email: "",
+		},
+		cli.Author{
+			Name:  "Sunny Xie",
+			Email: "",
+		},
+	}
+	app.HideHelp = true
+	app.HideVersion = true
+	cli.HelpFlag = cli.BoolFlag{
+		Name:        "help, h",
+		Usage:       "Show help",
+		Destination: &help,
+	}
 	//==================================================================================================================
 
 	// Argument Definitions - Need to add flags and positional arguments
@@ -21,14 +49,40 @@ func main() {
 		cli.StringFlag{
 			Name:  "produce-outputs, p",
 			Usage: "Produce test outputs based on `EXEC`",
-			Value: "",
 		},
+		cli.StringFlag{
+			Name:  "run-suite, r",
+			Usage: "Run test suite on `EXEC`",
+		},
+		cli.StringFlag{
+			Name:  "zip, z",
+			Usage: "Zip test suite to `ZIPFILE`",
+		},
+		cli.StringFlag{
+			Name:        "unzip, u",
+			Usage:       "Unzip test suite from `ZIPFILE`",
+		},
+		cli.BoolFlag{
+			Name:  "remove, rm",
+			Usage: "Remove test suite",
+			Destination: &rm,
+		},
+		cli.HelpFlag,
 	}
 	//==================================================================================================================
 
 	// App Action - Add basic app behaviour
 	//==================================================================================================================
 	app.Action = func(c *cli.Context) {
+		switch {
+		case rm:
+			fmt.Println("Removing...")
+		case help:
+			cli.ShowAppHelpAndExit(c, 0)
+		default:
+			cli.ShowAppHelpAndExit(c, 1)
+		}
+		fmt.Println("Done!")
 	}
 	//==================================================================================================================
 
@@ -37,7 +91,7 @@ func main() {
 	err := app.Run(os.Args)
 	// Only run if something real weird happens
 	if err != nil {
-		log.Fatalln(err)
+		//log.Fatalln(err)
 	}
 	//==================================================================================================================
 }
